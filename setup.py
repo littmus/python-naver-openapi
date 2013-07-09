df1131b3587e55502e33d1445bca9feb75b7d5df
SDW-bitave <sdw2648@gmail.com>
2013-07-09 22:52:09 +0900

    search api developing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+version = '0.1'
+
+setup(
+    name='python-naver-openapi'
+    version=version
+    author='Shin Dongwon'
+    author_email='sdw2648@gmail.com'
+    )
