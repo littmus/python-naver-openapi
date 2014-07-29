56be6338dcf689514f2683159befb12bea913a8e
littmus <sdw2648@gmail.com>
2014-07-29 12:14:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-version = '1.11'
+version = '1.1.2'
 
 setup(
     name='python-naver-openapi',
     version=version,
     description='Python wrapper for Naver OpenApi',
-    author='Shin Dongwon',
-    author_email='sdw2648@gmail.com',
+    author='littmus',
+    author_email='littmuss@gmail.com',
     url='https://github.com/littmus/python-naver-openapi',
     packages=['naver_openapi'],
     license='MIT License',
